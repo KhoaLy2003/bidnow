5134c34af683d1346d64fd31948c5c90d6509972
Hiep Nguyen <94773686+hiepnguyen06@users.noreply.github.com>
2026-05-10 20:37:32 +0700

    refactor(auth): use BASE64 encoded string value instead for JWT

--- backend/identity-service/src/main/java/com/bidnow/identity/security/JwtService.java
+++ backend/identity-service/src/main/java/com/bidnow/identity/security/JwtService.java
@@ -4,13 +4,13 @@ import com.bidnow.identity.domain.entity.User;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
-import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 @Service
@@ -23,7 +23,7 @@ public class JwtService {
     public JwtService(
             @Value("${jwt.secret}") String secret,
             @Value("${jwt.expiration}") long expiration) {
-        this.secretKey = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+        this.secretKey = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret));
         this.expiration = expiration;
     }
 
